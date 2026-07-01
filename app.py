740010cacb0e0d7f6150f9fc0a1cb2840c15aabd
suku <sukumarreddyp06@gmail.com>
2026-07-01 08:52:19 +0530

    Implement Greenhouse database sync

--- app.py
+++ app.py
@@ -53,4 +53,7 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()   # ← Add this line
+        print("Tables created!")
     app.run(debug=True)
